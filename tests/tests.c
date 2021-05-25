569af92d4ca1d1a4180fa927051feab9f788b52a
Martin Olivier <martin.olivier@live.fr>
2021-05-25 19:18:18 +0200

    feat: unit tests

--- tests/tests.c
+++ tests/tests.c
@@ -1,13 +1,14 @@
 #include "../cstring.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
 
-int main()
+Test(cstring, basic)
 {
     string i = string_create("hello");
     string_push_back(i, ' ');
     string_cat(i, "world!");
-    string_println(i);
+    cr_assert_str_eq(string_data(i), "hello world!");
     string_destroy(i);
-    return 0;
 }
